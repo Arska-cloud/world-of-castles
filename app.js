0cd817c7101c536065b956372a71f7457edce04b
Arska-cloud <arnaudeoliveira@gmail.com>
2023-06-09 12:41:25 +0200

    Base app created

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Server port and host
+app.listen(3000, () => {console.log('Serving on port 3000')});
+
+// Connecting views engine and folder
+app.set('view engine', 'ejs');
+app.set('views',path.join(__dirname,'views'));
+
+// Routes
+app.get('/', (req, res) => {
+    res.render('home')
+})
