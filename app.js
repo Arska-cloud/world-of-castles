53c1374fbd542c27fe87285eb071a8fad119f1a8
Arska-cloud <arnaudeoliveira@gmail.com>
2023-06-13 18:32:11 +0200

    Refactored routes and added flash alerts & session

--- app.js
+++ app.js
@@ -1,123 +1,78 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const path = require('path');
-const methodOverride = require('method-override');
-const ejsMate = require('ejs-mate');
-const Joi = require('joi');
-const Castle = require('./models/castle');
-const Review = require('./models/review');
-const wrapAsync = require('./utils/wrapAsync');
-const ExpressError = require('./utils/ExpressError');
-const {castleSchema} = require('./schemas.js');
-const {reviewSchema} = require('./schemas.js');
-
-
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+const Joi = require("joi");
+const session = require("express-session");
+const flash = require("connect-flash");
+// utils
+const wrapAsync = require("./utils/wrapAsync");
+const ExpressError = require("./utils/ExpressError");
+// routers
+const castles = require("./routes/castles");
+const reviews = require("./routes/reviews");
+// apps
 const app = express();
-app.use(express.urlencoded({ extended: true}));
-app.use(methodOverride('_method'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
+
+const sessionConfig = {
+  secret: "ñäñéñíñôñü",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  },
+};
+app.use(session(sessionConfig));
+app.use(flash());
+
+app.use((req, res, next) => {
+  res.locals.success = req.flash('success');
+  res.locals.error = req.flash('error');
+  next();
+});
 
 // Server port, host & database connection
-app.listen(3000, () => {console.log('Serving on port 3000')});
-mongoose.connect('mongodb://127.0.0.1:27017/world-of-castles', {useNewUrlParser: true, useUnifiedTopology: true})
+app.listen(3000, () => {
+  console.log("Serving on port 3000");
+});
+mongoose.connect("mongodb://127.0.0.1:27017/world-of-castles", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, "connection error"));
-db.once('open', () => {console.log('Database connected')});
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", () => {
+  console.log("Database connected");
+});
 
 // Connecting views engine and folder
-app.engine('ejs',ejsMate);
-app.set('view engine', 'ejs');
-app.set('views',path.join(__dirname,'views'));
-
-// This function will leave soon
-
-const validateCastle = (req, res, next) => {
-    const {error} = castleSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    } else {
-        next();
-    }
-}
-const validateReview = (req, res, next) => {
-    const {error} = reviewSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    } else {
-        next();
-    }
-}
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 // Routes
-app.get('/', (req, res) => {
-    res.render('home');
+app.use("/castles", castles);
+app.use("/castles/:id/reviews", reviews);
+
+app.get("/", (req, res) => {
+  res.render("home");
 });
-
-app.get('/castles', wrapAsync(async (req, res) => {
-    const castles = await Castle.find({});
-    res.render('castles/index', {castles});
-}));
-
-app.get('/castles/new', (req, res) => {
-    res.render('castles/new');
-});
-
-// Create, Edit, Update and delete routes for castles
-app.post('/castles', validateCastle, wrapAsync(async (req, res) => {
-    const castle = new Castle(req.body.castle);
-    await castle.save();
-    res.redirect(`/castles/${castle._id}`);
-}));
-
-app.get('/castles/:id/edit', wrapAsync(async (req, res) => {
-    const castle = await Castle.findById(req.params.id);
-    res.render('castles/edit', {castle});
-}));
-
-app.put('/castles/:id', validateCastle, wrapAsync(async (req, res) => {
-    const {id} = req.params;
-    const castle = await Castle.findByIdAndUpdate(id,{...req.body.castle});
-    res.redirect(`/castles/${castle._id}`);
-}));
-
-app.delete('/castles/:id', wrapAsync(async (req, res) => {
-    const {id} = req.params;
-    await Castle.findByIdAndDelete(id);
-    res.redirect('/castles');
-}));
-
-// Show route
-app.get('/castles/:id', wrapAsync(async (req, res) => {
-    const castle = await Castle.findById(req.params.id).populate('reviews');
-    res.render('castles/show', {castle});
-}));
-
-// Create & delete routes for reviews
-app.post('/castles/:id/reviews', validateReview, wrapAsync(async (req, res) => {
-    const castle = await Castle.findById(req.params.id);
-    const review = new Review(req.body.review);
-    castle.reviews.push(review);
-    await review.save();
-    await castle.save();
-    res.redirect(`/castles/${castle._id}`);
-}));
-
-app.delete('/castles/:id/reviews/:reviewId', wrapAsync(async (req, res) => {
-    const { id, reviewId } = req.params;
-    await Castle.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-    await Review.findByIdAndDelete(reviewId);
-    res.redirect(`/castles/${id}`);
-}));
 
 //Error catchers
-app.all('*', (req, res, next) => {
-    next(new ExpressError('Page Not Found', 404));
+app.all("*", (req, res, next) => {
+  next(new ExpressError("Page Not Found", 404));
 });
 
 app.use((err, req, res, next) => {
-    const {statusCode = 500, message = 'Something went wrong'} = err
-    if (!err.message) err.message = 'It looks like you got lost on your way to Santiago!'
-    res.status(statusCode).render('error', {err});
+  const { statusCode = 500 } = err;
+  if (!err.message) err.message = "Oh No, Something Went Wrong!";
+  res.status(statusCode).render("error", { err });
 });
---
+++ routes/castles.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const wrapAsync = require("../utils/wrapAsync");
+const Castle = require("../models/castle");
+const { castleSchema } = require("../schemas.js");
+const router = express.Router();
+const ExpressError = require("../utils/ExpressError");
+const Joi = require("joi");
+
+// Validation middleware
+const validateCastle = (req, res, next) => {
+  const { error } = castleSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+};
+
+// Routes
+router.get("/", wrapAsync(async (req, res) => {
+  const castles = await Castle.find({});
+  res.render("castles/index", { castles });
+})
+);
+
+router.get("/new", (req, res) => {
+  res.render("castles/new");
+});
+
+// Create, Edit, Update and delete routes for castles
+router.post("/", validateCastle, wrapAsync(async (req, res) => {
+  const castle = new Castle(req.body.castle);
+  await castle.save();
+  req.flash('success', 'Successfully built a castle.');
+  res.redirect(`/castles/${castle._id}`);
+})
+);
+
+router.get("/:id/edit", wrapAsync(async (req, res) => {
+  const castle = await Castle.findById(req.params.id);
+  if (!castle) {
+    req.flash('error', 'Seems like the castle is no longer there!');
+    return res.redirect("/castles");
+  }
+  res.render("castles/edit", { castle });
+})
+);
+
+router.put("/:id", validateCastle, wrapAsync(async (req, res) => {
+  const { id } = req.params;
+  const castle = await Castle.findByIdAndUpdate(id, { ...req.body.castle });
+  req.flash('success', 'Successfully updated the castle.');
+  res.redirect(`/castles/${castle._id}`);
+})
+);
+
+router.delete("/:id", wrapAsync(async (req, res) => {
+  const { id } = req.params;
+  await Castle.findByIdAndDelete(id);
+  req.flash('success', 'Successfully demolished the castle.');
+  res.redirect("/castles");
+})
+);
+
+// Show route
+router.get("/:id", wrapAsync(async (req, res) => {
+  const castle = await Castle.findById(req.params.id).populate("reviews");
+  if (!castle) {
+    req.flash('error', 'Seems like the castle is no longer there!');
+    return res.redirect("/castles");
+  }
+  res.render("castles/show", { castle });
+})
+);
+
+module.exports = router;
