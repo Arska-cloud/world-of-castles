66f0d6c6deafb4c4937ca870a75e5bfb2136a997
Arska-cloud <arnaudeoliveira@gmail.com>
2023-06-20 19:08:42 +0200

    App fully ready for deployment

--- app.js
+++ app.js
@@ -35,14 +35,14 @@ const store = MongoStore.create({
   mongoUrl: dbUrl,
   touchAfter: 24 * 60 * 60,
   crypto: {
-      secret: 'alsonotasecret!'
+      secret: process.env.SECRET
   }
 });
 
 const sessionConfig = {
   store,
   name: '_yklt',
-  secret: "notasecret",
+  secret: process.env.SECRET,
   resave: false,
   saveUninitialized: true,
   cookie: {
